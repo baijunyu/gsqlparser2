4130d5d6f879f9c212d4c8ebce6d4df5a105122c
baijunyu <52822237+baijunyu@users.noreply.github.com>
2024-10-29 21:56:44 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
-git remote add -f gsp_demo_java https://github.com/baijunyu/gsplparser.git
+git remote add -f gsp_demo_java https://oauth2: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@github.com/baijunyu/gsplparser.git
 
 git subtree add --prefix=gsp_demo_java gsp_demo_java gsp_demo_java_branch
